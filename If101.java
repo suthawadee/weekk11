395892d754b3332ccb0ff9ee4555279efd67bd3a
Taonahub <52515393+suthawadee@users.noreply.github.com>
2021-02-04 11:33:59 +0700

    Add files via upload

---
+++ If101.java
@@ -0,0 +1,12 @@
+package week12;
+
+public class If101 {
+	public static void main(String[]args) {
+		int num = 10;
+		if(num % 5 == 0) 
+		{
+			System.out.print("We are inside the if-block");
+		}
+		System.out.print("We are outside the if-block");
+	}
+}
